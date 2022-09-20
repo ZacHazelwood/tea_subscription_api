a69c6b124e8f84492ffcdc691678beaf20db832a
Zachary Hazelwood <zhaze001@gmail.com>
2022-09-20 10:39:55 -0600

    Add, Test: Tea Model and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_162801) do
+ActiveRecord::Schema.define(version: 2022_09_20_163300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2022_09_20_162801) do
     t.string "last_name"
     t.string "email"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "temperature"
+    t.integer "brew_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
