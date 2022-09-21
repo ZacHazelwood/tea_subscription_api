62ee121cc96d9b00eeacc471d6e2807956a354e3
Zachary Hazelwood <zhaze001@gmail.com>
2022-09-21 12:17:11 -0600

    Add, Test: Subscriptions GET request happy path

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
+  def index
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
+  end
+
   def create
     subscription = Subscription.new(subscription_params)
       if subscription.save
